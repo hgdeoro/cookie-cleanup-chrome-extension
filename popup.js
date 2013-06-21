37a6cef8487dd972d79821ee826119ba2ea9764e
Horacio G. de Oro <hgdeoro@gmail.com>
2013-06-21 15:54:31 -0300

    Populate white lists from local storage

--- popup.js
+++ popup.js
@@ -1,17 +1,29 @@
 // http://www.w3schools.com/jsref/jsref_obj_regexp.asp
-var WHITE_LIST_REGEXS = [
-// Google & co
-new RegExp("^(.*\\.|)google\\.com$", "i"),
-		new RegExp("^(.*\\.|)youtube\\.com$", "i"), ];
+var WHITE_LIST_REGEXS = [];
+
+var INITIAL_WHITE_LIST = [ "google.com", "youtube.com" ];
 
 var utils = {
+	cleanHtml : function(tag, text) {
+		/**
+		 * Clean up the HTML (the UI)
+		 */
+		document.body.innerHTML = "";
+	},
+
 	addHtml : function(tag, text) {
+		/**
+		 * Add an HTML element to the UI
+		 */
 		var new_elem = document.createElement(tag);
 		new_elem.appendChild(document.createTextNode(text));
 		document.body.appendChild(new_elem);
 	},
 
 	shouldKeep : function(a_cookie) {
+		/**
+		 * Return true|false, if the cookie should be keeped or not
+		 */
 		// a_cookie.name,
 		// a_cookie.value,
 		// a_cookie.domain;
@@ -24,6 +36,9 @@ var utils = {
 	},
 
 	removeCookie : function(cookie) {
+		/**
+		 * Removes a cookie.
+		 */
 		console.info("Removing cookie " + cookie);
 		var url = "http" + (cookie.secure ? "s" : "") + "://" + cookie.domain
 				+ cookie.path;
@@ -34,21 +49,53 @@ var utils = {
 		});
 	},
 
+	populateWhiteList : function() {
+		/**
+		 * Populate the WHITE_LIST_REGEXS with RegExp objects from the
+		 * configuration
+		 */
+		if (WHITE_LIST_REGEXS.length != 0)
+			return;
+
+		var white_list = JSON.parse(localStorage.getItem('white_list'));
+		console.info("Loaded `white_list` = " + white_list);
+
+		WHITE_LIST_REGEXS = white_list.map(function(wl_item) {
+			var regex_item_string = '^(.*\\.|)'
+					+ wl_item.split('.').join('\\.') + '$';
+			return new RegExp(regex_item_string, "i");
+		});
+	},
+
+	initLocalStorage : function() {
+		/**
+		 * Initializes local storage with default values
+		 */
+		if (localStorage.getItem('white_list') != null)
+			return;
+
+		console.info("Creating initial white_list...");
+		localStorage.setItem('white_list', JSON.stringify(INITIAL_WHITE_LIST));
+	},
+
 };
 
 var cookiesCleaner = {
 
-	preview : function(do_cleanup) {
-		//
-		// do_cleanup == true -> do the cleanup
-		// do_cleanup != true -> preview
-		//
-		if (localStorage.getItem('white_list') == null) {
-			localStorage.setItem('white_list', JSON.stringify([ 'google.com',
-					'youtube.com' ]));
-		}
+	_preview_or_cleanup : function(do_cleanup) {
+		/**
+		 * Preview or cleanup the cookies.
+		 * 
+		 * do_cleanup == true -> CLEANUP
+		 * 
+		 * do_cleanup != true -> PREVIEW
+		 */
 		chrome.cookies.getAll({}, function(all_the_cookies) {
-			utils.addHtml('p', JSON.parse(localStorage.getItem('white_list')));
+
+			/*
+			 * Header
+			 */
+
 			if (do_cleanup == true) {
 				console.info("Starting cleanup...");
 				utils.addHtml('h2', 'Keeping...');
@@ -58,6 +105,10 @@ var cookiesCleaner = {
 			}
 			var keeped_domains = [];
 			var removed_domains = [];
+
+			/*
+			 * List cookies to keep/kept
+			 */
 
 			for ( var i = 0; i < all_the_cookies.length; i++) {
 				var a_cookie = all_the_cookies[i];
@@ -71,6 +122,10 @@ var cookiesCleaner = {
 					}
 				}
 			}
+
+			/*
+			 * List cookies to remove/removed
+			 */
 
 			if (do_cleanup == true) {
 				utils.addHtml('h2', 'Removing...');
@@ -93,6 +148,10 @@ var cookiesCleaner = {
 				}
 			}
 
+			/*
+			 * Add the footer with buttons
+			 */
+
 			if (do_cleanup == true) {
 				utils.addHtml('h1', 'Cleanup DONE!');
 			} else {
@@ -108,14 +167,27 @@ var cookiesCleaner = {
 		});
 	},
 
-	cleanup : function() {
+	preview : function() {
+		/**
+		 * Preview
+		 */
+		utils.initLocalStorage();
+		utils.populateWhiteList();
 		document.body.innerHTML = "";
-		cookiesCleaner.preview(true);
+		cookiesCleaner._preview_or_cleanup(false); // do_cleanup: false
+	},
+
+	cleanup : function() {
+		/**
+		 * Clean Up
+		 */
+		document.body.innerHTML = "";
+		cookiesCleaner._preview_or_cleanup(true); // do_cleanup: true
 	},
 
 };
 
 // Run our kitten generation script as soon as the document's DOM is ready.
 document.addEventListener('DOMContentLoaded', function() {
-	cookiesCleaner.preview(false);
+	cookiesCleaner.preview();
 });
